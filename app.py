99ec8e31d4cbb93333c8dc45d60f11260f01c85d
Willo-1982 <d.61williams@googlemail.com>
2025-06-26 15:38:06 +0100

    app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if len(player_data) >= 8:
     course_rating = player_data['Course Rating'].iloc[0]
     slope_rating = player_data['Slope Rating'].iloc[0]
     course_handicap = calculate_course_handicap(handicap_index, course_rating, slope_rating)
-    st.write(f'{player}'s Handicap Index: {handicap_index:.2f}')
+    st.write(f"{player}'s Handicap Index: {handicap_index:.2f}")
     st.write(f'{player}'s Course Handicap: {course_handicap:.2f}')
 else:
     st.write('Not enough scores to calculate handicap.')
